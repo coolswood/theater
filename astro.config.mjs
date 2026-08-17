3acaba199868854e1508219fce41e9ea7104466a
coolswood <coolswood@yandex.ru>
2026-08-17 12:36:37 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://teatterilogrus.fi',
   adapter: vercel(),
+  build: {
+    inlineStylesheets: 'always',
+  },
 
   vite: {
     plugins: [tailwindcss()]
